b099ccb98b782877a91f1a52f10beffddeb11ada
unknown <nabilasoma@gmail.com>
2023-05-19 22:45:46 +0600

    added some imgage in category page

--- src/category/Category.jsx
+++ src/category/Category.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Category.css'
 import { Link } from 'react-router-dom';
 
 const Category = () => {
     const [toggle, setToggle] = useState(1);
+    const [toys, setToys] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/allToys')
+        .then(res => res.json())
+        .then(data => setToys(data))
+    },[])
+
+    console.log(toys);
 
     const updateToggle = id => {
         setToggle(id)
@@ -12,14 +21,16 @@ const Category = () => {
 
     return (
         <div>
-            <h3 className='text-red-600 font-bold text-3xl mb-5'>Choose Your Toys By Category</h3>
+            
+            <h3 className='text-red-600 font-bold text-3xl mb-5'>Choose Your Toys By Category{toys.length}</h3>
                 <button onClick={() => updateToggle(1)} className="btn btn-active">Bus Toys</button>
                 <button onClick={() => updateToggle(2)} className="btn btn-active btn-primary">Truck Toys</button>
                 <button onClick={() => updateToggle(3)} className="btn btn-active btn-secondary">Car Toys</button>
             
 
             <div className={toggle === 1 ? "show-content" : "content"}>
-                <div className="card mt-5 card-compact w-96 bg-base-100 mx-auto shadow-xl">
+              <div className='flex gap-6'>
+              <div className="card mt-5 card-compact h-96 w-96 bg-base-100 mx-auto shadow-xl">
                     <figure><img src="https://i.ibb.co/L9VpMcW/img6.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -29,7 +40,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto mt-5 card card-compact w-96 bg-base-100 shadow-xl">
+                <div className="mx-auto mt-5 card h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/p2npCVT/img7.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -39,7 +50,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto mt-5 card card-compact w-96 bg-base-100 shadow-xl">
+                <div className="mx-auto mt-5 card h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/TPtVxf7/img8.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -49,9 +60,11 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
+              </div>
             </div>
             <div className={toggle === 2 ? "show-content" : "content"}>
-            <div className="mx-auto card mt-5 card-compact w-96 bg-base-100 shadow-xl">
+           <div className='flex gap-6'>
+           <div className="mx-auto card mt-5 h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/27Wtm03/img11.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -61,7 +74,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto mt-5 card card-compact w-96 bg-base-100 shadow-xl">
+                <div className="mx-auto mt-5 card h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/G9qzcrz/img10.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -71,7 +84,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto mt-5 card card-compact w-96 bg-base-100 shadow-xl">
+                <div className="mx-auto mt-5 h-96 card card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/CJ0tGcD/img9.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -82,8 +95,10 @@ const Category = () => {
                     </div>
                 </div>
             </div>
+           </div>
             <div className={toggle === 3 ? "show-content" : "content"}>
-            <div className="mx-auto card mt-5 card-compact w-96 bg-base-100 shadow-xl">
+            <div className='flex gap-6'>
+            <div className="mx-auto card mt-5 h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/kBLwGCN/img2.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -93,7 +108,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto mt-5 card card-compact w-96 bg-base-100 shadow-xl">
+                <div className="mx-auto mt-5 card h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/JjVF4bn/img1.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -103,7 +118,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
-                <div className="mx-auto card mt-5 card-compact w-96 bg-base-100 shadow-xl">
+                <div className="mx-auto card mt-5 h-96 card-compact w-96 bg-base-100 shadow-xl">
                     <figure><img src="https://i.ibb.co/sQjVB9D/img12.jpg" alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title"></h2>
@@ -113,6 +128,7 @@ const Category = () => {
                         </div>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
 
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -37,14 +37,14 @@ const Navbar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><Link>Home</Link></li>
-                    <li><Link to='/blog'>Blog</Link></li>
-                    <li><Link to='/allToys'>All Toys</Link></li>
+                    <li className='font-bold'><Link>Home</Link></li>
+                    <li className='font-bold'><Link to='/blog'>Blog</Link></li>
+                    <li className='font-bold'><Link to='/allToys'>All Toys</Link></li>
                     {
                         user ? <>
-                           <li><Link to='/addAToy'>Add A Toy</Link></li>
-                            <li><Link to='/myToy'>My Toy</Link></li>
-                            <Link><button onClick={handleLogout} className="btn btn-link">Logout</button></Link>
+                           <li className='font-bold'><Link to='/addAToy'>Add A Toy</Link></li>
+                            <li className='font-bold'><Link to='/myToy'>My Toy</Link></li>
+                            <Link className='font-bold'><button onClick={handleLogout} className="btn btn-link">Logout</button></Link>
                             <a><img style={{ width: '40px' }} src={user1} alt="" /></a>
                         </> : <li><Link to='/login'>Login</Link></li>
                     }
