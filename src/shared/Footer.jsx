98c21aae5ce750ead586d6b099918d8783fc194e
unknown <nabilasoma@gmail.com>
2023-05-21 12:10:58 +0600

    changes some code on footer

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     return (
         <footer className="bg-gray-900 text-white py-8">
             <div className="container mx-auto px-4">
-                <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                     <div className="col-span-1">
                     <img
                             src={logo}
